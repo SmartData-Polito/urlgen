5a360d7e7c4f1ce101cccf9c9c4212a896578b80
rodolfovalentim <rodolfovieiravalentim@gmail.com>
2022-06-01 18:21:43 +0200

    fix name output file

--- generate.py
+++ generate.py
@@ -3,7 +3,7 @@ import random
 baseurl = "www.example.com/position/search?q={},{}&apikey={}&language=it\n"
 urls = []
 
-with open(r'./sales.txt', 'w') as fp:
+with open(r'./example-dataset.txt', 'w') as fp:
     for i in range(20000):
         fp.write(baseurl.format(random.randint(454365384, 464365384)/10000000, random.randint(86266474, 87266474)/10000000, random.getrandbits(128)))
 
